821526803d8e4b8e00d556a9c78d9ebf49b2556b
Unknown <dhorvath19@gmail.com>
2019-03-12 18:01:11 +0100

    Added extra layers and fixed param update

--- binary_neuron/model.py
+++ binary_neuron/model.py
@@ -1,22 +1,34 @@
 import tensorflow as tf
+from binary_neuron.utils import binarize, hard_tanh
 
 
 class Model(object):
     def __init__(self):
-        self.W = tf.Variable(tf.random.uniform([10, 2], dtype=tf.float32))
-        self.b = tf.Variable(tf.random.uniform([10], dtype=tf.float32))
+        self.weights = [
+            tf.Variable(tf.random.uniform([10, 2], dtype=tf.float32,
+                                          minval=tf.constant(-1, dtype=tf.float32))),
+            tf.Variable(tf.random.uniform([1, 10], dtype=tf.float32,
+                                          minval=tf.constant(-1, dtype=tf.float32)))
+        ]
 
     def params(self):
-        return [self.W, self.b]
+        return self.weights
 
     def update(self, gradients, learning_rate):
-        # for idx, param in self.params():
-        #     print(param.numpy())
-        self.W.assign_sub(gradients[0] * learning_rate)
-        self.b.assign_sub(gradients[1] * learning_rate)
+        for idx, weight in enumerate(self.weights):
+            weight.assign_sub(gradients[idx] * learning_rate)
 
     def __call__(self, x):
-        return tf.linalg.matmul(self.W, x) + self.b
+        for weight in self.weights[:-1]:
+            # weight = binarize(weight)
+            x = tf.linalg.matmul(weight, x)
+            # x = binarize(x)
+
+        last_weight = self.weights[-1]
+        # last_weight = binarize(last_weight)
+
+        out = tf.linalg.matmul(last_weight, x)
+        return tf.reshape(out, [-1])
 
 
 if __name__ == "__main__":
@@ -26,6 +38,5 @@ if __name__ == "__main__":
     # sess = tf.Session()  # graph=graph)
     # with sess.as_default():
     model = Model()
-    print(model.W.numpy())
-    model.update([tf.random.uniform([10]), tf.random.uniform([10])], 1)
-    print(model.W.numpy())
+    model.update([tf.random.uniform([10, 2])], 1)
+    print('done')
--- binary_neuron/train.py
+++ binary_neuron/train.py
@@ -6,25 +6,27 @@ def loss(predicted_y, desired_y):
     return tf.reduce_mean(tf.square(predicted_y - desired_y))
 
 
-def backward(model, inputs, outputs, loss, learning_rate):
+def backward(model, inputs, targets, loss, learning_rate):
     with tf.GradientTape() as t:
-        current_loss = loss(model(inputs), outputs)
+        current_loss = loss(model(inputs), targets)
+    # print('Inputs: ', inputs.numpy())
+    # print('Target: ', targets.numpy())
+    # print('Predictions: ', model(inputs).numpy())
+    # print('Losses: ', current_loss)
+    # print('Params: ', model.params())
+
     gradients = t.gradient(current_loss, model.params())
-    # model.W.assign_sub(learning_rate * dW)
-    # model.b.assign_sub(learning_rate * db)
+
+    # print('Gradients: ', gradients)
     model.update(gradients, learning_rate)
 
 
 def train(model, inputs, targets, epochs=10):
-    # Collect the history of W-values and b-values to plot later
-    Ws, bs = [], []
     for epoch in range(epochs):
         current_loss = []
         for i in range(inputs.shape[0]):
             input = tf.convert_to_tensor(inputs[np.newaxis, i].T, dtype=tf.float32)
             target = tf.convert_to_tensor(targets[i], dtype=tf.float32)
-            Ws.append(model.W.numpy())
-            bs.append(model.b.numpy())
             current_loss.append(loss(model(input), target))
 
             backward(model, input, target, loss, learning_rate=0.1)
