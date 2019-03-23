56a1a5fc89e42c71d6963545a016d39451b4a38c
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2019-03-23 10:46:13 +0100

    update 2

--- binary_neuron/Germans/utils.py
+++ binary_neuron/Germans/utils.py
@@ -13,7 +13,7 @@ def binarize_(tensor):
 @tf.custom_gradient
 def binarize(x):
     def grad(dy):
-        return tf.clip_by_value(dy, -1.0, 1.0)
+        return tf.clip_by_value(dy, -0.1, 0.1)
 
     return tf.sign(x), grad
 
--- binary_neuron/main.py
+++ binary_neuron/main.py
@@ -6,4 +6,9 @@ from binary_neuron.model import Model
 from binary_neuron.train import train
 
 model = Model()
+<<<<<<< HEAD
 train(model, data, labels, epochs=50)
+=======
+
+train(model, data, labels, epochs=100)
+>>>>>>> parent of 97db9b1... Update
