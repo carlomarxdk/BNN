31e0eca5dbb5e7b6f8ad7cd84ea9ee41d931682d
Daniel Horvath <dhorvath19@gmail.com>
2019-05-10 15:31:06 +0200

    Fixed layers

--- binary_neuron/layers.py
+++ binary_neuron/layers.py
@@ -19,18 +19,20 @@ def binaryLinearLayer(num_out, name=None):
 
 
 class BinaryLinearLayer(tf.keras.layers.Layer):
-    def __init__(self, num_out):
+    def __init__(self, num_out, binarize_input=True):
         super(BinaryLinearLayer, self).__init__()
         self.num_out = num_out
+        self.binarize_input = binarize_input
 
     def build(self, input_shape):
         self.weight = tf.get_variable('weight', [int(input_shape[-1]), self.num_out],
                                       initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
 
     def call(self, x):
-        x_binary = binarize(x)
-        weight_binary = binarize(self.weight)
-        return tf.matmul(x_binary, weight_binary)
+        if self.binarize_input:
+            x = binarize(x)
+        weight = binarize(self.weight)
+        return tf.matmul(x, weight)
 
 
 class HardTanH(tf.keras.layers.Layer):
--- binary_neuron/model.py
+++ binary_neuron/model.py
@@ -1,43 +1,45 @@
 import tensorflow as tf
 
-from binary_neuron.layers import BinaryLinearLayer
+from binary_neuron.layers import BinaryLinearLayer, HardTanH, SoftMax
 from binary_neuron.utils import binarize, hard_tanh
 
 
-class Model(object):
-    def __init__(self, classes=2):
-        self.weights = [
-            tf.get_variable('weight', [7, 200], initializer=tf.contrib.layers.xavier_initializer()),
-            tf.get_variable('weight', [200, 200], initializer=tf.contrib.layers.xavier_initializer()),
-            tf.get_variable('weight', [200, classes], initializer=tf.contrib.layers.xavier_initializer())
-        ]
+# class Model(object):
+#     def __init__(self, classes=2):
+#         self.weights = [
+#             tf.get_variable('weight', [7, 200], initializer=tf.contrib.layers.xavier_initializer()),
+#             tf.get_variable('weight', [200, 200], initializer=tf.contrib.layers.xavier_initializer()),
+#             tf.get_variable('weight', [200, classes], initializer=tf.contrib.layers.xavier_initializer())
+#         ]
+#
+#         self.trainable_variables = self.weights
+#
+#     def __call__(self, x):
+#         for weight in self.weights[:-1]:
+#             weight = binarize(weight)
+#             x = tf.linalg.matmul(x, weight)
+#             x = hard_tanh(x)
+#
+#         last_weight = binarize(self.weights[-1])
+#
+#         x = tf.linalg.matmul(x, last_weight)
+#
+#         out = tf.nn.softmax(x)
+#         return out
 
-        self.trainable_variables = self.weights
 
-    def __call__(self, x):
-        for weight in self.weights[:-1]:
-            weight = binarize(weight)
-            x = tf.linalg.matmul(x, weight)
-            x = hard_tanh(x)
-
-        last_weight = binarize(self.weights[-1])
-
-        x = tf.linalg.matmul(x, last_weight)
-
-        out = tf.nn.softmax(x)
-        return out
-
-
-# def Model(classes=2):
-#     model = tf.keras.Sequential([
-#         BinaryLinearLayer(10),
-#         HardTanH(),
-#         BinaryLinearLayer(10),
-#         HardTanH(),
-#         BinaryLinearLayer(classes),
-#         SoftMax()
-#     ])
-#     return model
+def Model(classes=2):
+    model = tf.keras.Sequential([
+        BinaryLinearLayer(50, binarize_input=False),
+        HardTanH(),
+        BinaryLinearLayer(100),
+        HardTanH(),
+        BinaryLinearLayer(100),
+        HardTanH(),
+        BinaryLinearLayer(classes),
+        SoftMax()
+    ])
+    return model
 
 
 if __name__ == "__main__":
